ee103df24c8eee443429bf6d0a4b8127c75826f2
Caio Yoshio Itiroko <caio.itiroko@tagview.com.br>
2019-04-11 20:43:29 -0300

    Add commonIdentifier to ingredients

--- constants.js
+++ constants.js
@@ -11,11 +11,11 @@ const EGG = "Ovo";
 const CHEESE = "Queijo";
 
 const ingredients = [
-  { id: uuid(), name: LETTUCE, price: 0.4 },
-  { id: uuid(), name: BACON, price: 2 },
-  { id: uuid(), name: MEAT_BURGER, price: 3 },
-  { id: uuid(), name: EGG, price: 0.8 },
-  { id: uuid(), name: CHEESE, price: 1.5 },
+  { id: uuid(), name: LETTUCE, price: 0.4, commonIdentifier: "lettuce" },
+  { id: uuid(), name: BACON, price: 2, commonIdentifier: "bacon" },
+  { id: uuid(), name: MEAT_BURGER, price: 3, commonIdentifier: "meatBurger" },
+  { id: uuid(), name: EGG, price: 0.8, commonIdentifier: "egg" },
+  { id: uuid(), name: CHEESE, price: 1.5, commonIdentifier: "cheese" },
 ];
 
 const snacks = [
